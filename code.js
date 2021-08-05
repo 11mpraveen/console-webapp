54eb2871d1fd1cdb74d9b5db49ed5de33b68a45d
11mpraveen <82409056+11mpraveen@users.noreply.github.com>
2021-08-05 11:04:47 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,13 @@
+var first_name = "Manasa";
+var last_name = "Praveen";
+var a = 5;
+var b = 10;
+
+//print("My first console project")
+
+console.log("My first console project");
+console.log(first_name + last_name);
+console.log( a + b );
+console.log(a);
+console.log( a*b);
+console.log(b/a);
